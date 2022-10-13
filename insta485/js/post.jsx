c096dce670c44bc2429e3a38b1e701f8286f9b8a
Sparsh Tripathi <sparsht@umich.edu>
2022-10-12 23:48:44 -0400

    React/JS part completed

---
+++ insta485/js/post.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Post extends React.Component {
+  /* Display image and post owner of a single post
+   */
+  constructor(props) {
+    // Initialize mutable state
+    super(props);
+    this.state = { imgUrl: "", owner: "" };
+  }
+
+  componentDidMount() {
+    // This line automatically assigns this.props.url to the const variable url
+    const { url } = this.props;
+    // Call REST API to get the post's information
+    fetch(url, { credentials: "same-origin" })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          imgUrl: data.imgUrl,
+          owner: data.owner,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    // This line automatically assigns this.state.imgUrl to the const variable imgUrl
+    // and this.state.owner to the const variable owner
+    const { imgUrl, owner } = this.state;
+    // Render post image and post owner
+    return (
+      <div className="post">
+        <img src={imgUrl} alt="post_image" />
+        <p>{owner}</p>
+      </div>
+    );
+  }
+}
+
+Post.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+export default Post;
